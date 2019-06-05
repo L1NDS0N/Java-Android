e253429f053c1b47c7ce45efbc06ab78822f1a0e
Lindson França <l1nds0n@hotmail.com>
2019-06-05 16:12:13 -0300

    Initial commit

--- app/src/main/java/br/senai/rn/catalogo/models/Catalogo.java
+++ app/src/main/java/br/senai/rn/catalogo/models/Catalogo.java
@@ -1,4 +1,13 @@
 package br.senai.rn.catalogo.models;
 
 public class Catalogo {
+    private String nome;
+    private String preco;
+    private String fabricante;
+
+
+
+    public void Catalogo(){
+    }
+
 }
